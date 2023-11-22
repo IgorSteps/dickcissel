131bab8225af822ce959bdc020663f9696b7d339
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-11-22 17:50:35 +0000

    Improvements

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,31 +6,34 @@ export default function (props) {
     <div className="login-form-container">
       <form className="login-form">
         <div className="login-form-content">
-          <h3 className="login-form-title">Sign In</h3>
+          <h3 className="login-form-title">Login</h3>
+
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
+              autoComplete="username"
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
             />
           </div>
+
           <div className="form-group mt-3">
             <label>Password</label>
             <input
+              autoComplete="current-password"
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
             />
           </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
+
+          <div className="d-grid gap-2 mt-4">
+            <button type="submit" className="btn btn-success">
+              Login
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
+
         </div>
       </form>
     </div>
