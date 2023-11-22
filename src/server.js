f3d5ec4a5bfed30b9a2ed523bb23d8086e357763
Igor Stepanenko <84919282+IgorSteps@users.noreply.github.com>
2023-11-22 16:33:18 +0000

    DIC-6: Add miragejs server (#1)
    * Add miragejs server
    
    * Add more routes

---
+++ src/server.js
@@ -0,0 +1,56 @@
+// src/server.js
+import { createServer, Model } from "miragejs"
+
+export function makeServer({ environment = "test" } = {}) {
+  let server = createServer({
+    environment,
+
+    models: {
+      users: Model,
+      observation: Model,
+    },
+
+    routes() {
+      this.namespace = "api"
+
+       // Login
+       this.post('/login', (schema, request) => {
+        const { username, password } = JSON.parse(request.requestBody);
+        // Authentication logic here
+        // Return user data or error
+      });
+
+      // Register
+      this.post('/register', (schema, request) => {
+        const newUser = JSON.parse(request.requestBody);
+        // Registration logic here
+        // Return user data or error
+      });
+
+      // Add observation
+      this.post('/observations', (schema, request) => {
+        const observation = JSON.parse(request.requestBody);
+        // Add observation logic here
+        // Return observation data
+        return schema.observations.create(observation);
+      });
+
+      // View observation
+      this.get('/observations/:id', (schema, request) => {
+        let id = request.params.id;
+        // Fetch observation logic here
+        return schema.observations.find(id);
+      });
+
+      // Edit observation
+      this.put('/observations/:id', (schema, request) => {
+        let id = request.params.id;
+        const newObservation = JSON.parse(request.requestBody);
+        // Update observation logic here
+        return schema.observations.find(id).update(newObservation);
+      });
+    },
+  })
+
+  return server
+}
