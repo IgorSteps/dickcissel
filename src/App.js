e343ff161f7a97938d6f887a872ac2b191a040bf
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-11-24 20:42:07 +0000

    First draft

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Auth from "./components/auth/auth"
 import Home from "./components/home/home"
+import Observations from "./components/observations/observations"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/observations" element={<Observations />} />
       </Routes>
     </BrowserRouter>
   );
