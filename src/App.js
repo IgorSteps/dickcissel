eee32aa3bb2169b6bf4b9af6a4c4bb96da23ade1
BlazeWing20 <123957086+BlazeWing20@users.noreply.github.com>
2023-12-04 14:37:16 +0000

    Education pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
 import Auth from "./components/auth/auth"
-import {AuthProvider} from './components/context/authContext';
+import { AuthProvider } from './components/context/authContext';
 import Home from "./components/home/home"
 import Observations from "./components/observations/observations"
 import BlogPage from './components/blog/blog';
+import Education from './components/education/education';
 
 function App() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/observations" element={<Observations />} />
-          <Route path="/blog" element={<BlogPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/observations" element={<Observations />} />
+                    <Route path="/blog" element={<BlogPage />} />
+                    <Route path="/education" element={<Education />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
-
 export default App;
+
