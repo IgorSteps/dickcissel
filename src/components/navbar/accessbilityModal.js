ce9f1a9610106a9536c733f28d522a2f413bf431
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-12-07 13:46:05 +0000

    Add dark mode

--- src/components/navbar/accessbilityModal.js
+++ src/components/navbar/accessbilityModal.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import React, { useState, useEffect } from 'react';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import RangeSlider from 'react-bootstrap-range-slider';
@@ -20,6 +21,16 @@ function AccessbilityModal({show, handleClose}) {
             setProperty('--dynamic-font-size', `${fontSize}px`);
     }, [fontSize]);
       
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+    };
+
+    useEffect(() => {
+        document.
+            documentElement.
+            setAttribute('data-bs-theme', theme);
+    }, [theme]);
 
     return (
         <Modal show={show} onHide={handleClose}>
@@ -34,6 +45,11 @@ function AccessbilityModal({show, handleClose}) {
                     value={fontSize}
                     onChange={e => handleFontSizeChange(e.target.value)}
                 />
+
+                <p>Choose colour scheme:</p>
+                <Button onClick={toggleTheme}>
+                    {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+                </Button>
             </Modal.Body>
 
             <Modal.Footer>
