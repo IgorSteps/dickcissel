10a8a6872512bd6ec9961ff8b5d117b18adf0201
Igor Stepanenko <84919282+IgorSteps@users.noreply.github.com>
2023-12-07 18:59:27 +0000

    DIC-19: Add colour schemes to accessibility. (#12)
    * Add dark mode
    
    * High contrast theme
    
    * Cleanup

--- src/components/navbar/accessbilityModal.js
+++ src/components/navbar/accessbilityModal.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import React, { useState, useEffect } from 'react';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import RangeSlider from 'react-bootstrap-range-slider';
@@ -20,6 +21,23 @@ function AccessbilityModal({show, handleClose}) {
             setProperty('--dynamic-font-size', `${fontSize}px`);
     }, [fontSize]);
       
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const toggleLightTheme = () => {
+        setTheme('light');
+    };
+    const toggleDarkTheme = () => {
+        setTheme('dark');
+    };
+    const toggleHRTheme = () => {
+        setTheme('high-contrast');
+    };
+
+    useEffect(() => {
+        document.
+            documentElement.
+            setAttribute('data-bs-theme', theme);
+            localStorage.setItem('theme', theme);
+    }, [theme]);
 
     return (
         <Modal show={show} onHide={handleClose}>
@@ -34,6 +52,17 @@ function AccessbilityModal({show, handleClose}) {
                     value={fontSize}
                     onChange={e => handleFontSizeChange(e.target.value)}
                 />
+
+                <p>Choose colour scheme:</p>
+                <Button className='mx-2' onClick={toggleLightTheme}>
+                    Light
+                </Button>
+                <Button className='mx-2' onClick={toggleDarkTheme}>
+                    Dark
+                </Button>
+                <Button className='mx-2' onClick={toggleHRTheme}>
+                    High Contrast
+                </Button>
             </Modal.Body>
 
             <Modal.Footer>
