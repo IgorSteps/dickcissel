b87e9c9f818e5ca057970b0a6490bbf7067a3a74
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-12-08 20:56:08 +0000

    FAQs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/home/home"
 import Observations from "./components/observations/observations"
 import BlogPage from './components/blog/blog';
 import AdminPage from './components/admin/admin';
+import FAQ from './components/help/faq';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/observations" element={<Observations />} />
           <Route path="/blog" element={<BlogPage />} />
+          <Route path="/faq" element={<FAQ />} />
           <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -38,7 +38,7 @@ function BasicNavbar() {
             <Nav.Link href="http://localhost:3000/blog">Blog</Nav.Link>
             <NavDropdown title="Help" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.3">How To Use</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">FAQ</NavDropdown.Item>
+              <NavDropdown.Item href="http://localhost:3000/faq">FAQ</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Contact Us</NavDropdown.Item>
             </NavDropdown>
           </Nav>
